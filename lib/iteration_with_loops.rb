5a7584a92ce64df83a9f66b54dee6f6cb400a411
Warren Niu <warrenniu@gmail.com>
2020-09-16 18:20:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
     if src[row_index][element_index].even?
-      p src[row_index][element_index].even?
+      p src[row_index][element_index]
     element_index +=1
   end
   row_index += 1
